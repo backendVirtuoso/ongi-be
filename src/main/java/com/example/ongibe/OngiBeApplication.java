32c245d04dc83252ab7874c5c9cb79e544c17d97
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2026-03-21 18:10:21 +0900

    Initial commit

---
+++ src/main/java/com/example/ongibe/OngiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ongibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OngiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ongibe/OngiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ongibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
